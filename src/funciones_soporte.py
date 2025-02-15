7863c872be113ecd3aedc2ed665cbfa870e0fc51
Gabriela Jiménez Conde <gabrielajimenezconde@gmail.com>
2025-02-16 00:27:17 +0100

    Avanzamos

--- src/funciones_soporte.py
+++ src/funciones_soporte.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from pandas.api.types import is_datetime64_any_dtype as is_datetime
 
 # Función info de dataframes 
 def info_df(dataframe):
@@ -21,3 +22,49 @@ def info_df(dataframe):
     info_df["%_nulos"] = round((dataframe.isnull().sum()/dataframe.shape[0])*100, 2)
 
     return info_df
+
+# Creamos una función que nos modifica el tipo de dato de las columnas que le pasemos a tipo fecha
+def data_fechas(dataframe, columnas):
+    """
+    Convierte las columnas especificadas de un DataFrame a tipo datetime.
+
+    Args:
+        dataframe (pd.DataFrame): DataFrame que contiene las columnas a modificar.
+        columnas (list): Lista de nombres de las columnas a convertir a datetime.
+
+    Returns:
+        pd.DataFrame: DataFrame con las columnas especificadas convertidas a datetime.
+    """
+    for col in columnas:
+        dataframe[col] = pd.to_datetime(dataframe[col])
+    return dataframe
+
+
+# Creamos una función que realiza las mismas funciones que el describe, pero con más estadísticos, solo para elementos numéricos
+def estadisticos_numericas_df(dataframe, lista_columnas):
+    valores_curtosis = []
+    valores_asimetria = []
+    valores_moda = []
+    #creamos un bucle for para calcular los valores de la curtosis y la asimetría
+    for columna in lista_columnas:
+        if dataframe[columna].dtype == int or dataframe[columna].dtype == float:
+
+            curtosis = round(dataframe[columna].kurtosis(), 2)
+            asimetria = round(dataframe[columna].skew(),2)
+            moda = dataframe[columna].mode()
+
+            valores_curtosis.append(curtosis)
+            valores_asimetria.append(asimetria)
+            valores_moda.append(float(moda.iloc[0]))
+            
+        elif is_datetime(dataframe[columna]):
+            valores_curtosis.append(np.nan)
+            valores_asimetria.append(np.nan)
+            valores_moda.append(float(moda.iloc[0]))
+            
+    estadísticos_generales = round(dataframe.describe().T, 2)
+    estadísticos_generales["Curtosis"] = valores_curtosis
+    estadísticos_generales["Coef_asimetria"] = valores_asimetria
+    estadísticos_generales["Moda"] = valores_moda
+
+    return estadísticos_generales
