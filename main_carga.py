2e4aa8b786887b731d502bff513975e1038c3e7b
Gabriela Jiménez Conde <gabrielajimenezconde@gmail.com>
2025-02-25 13:18:44 +0100

    estamos con .py carga

--- main_carga.py
+++ main_carga.py
@@ -2,7 +2,6 @@
 import pandas as pd
 import numpy as np
 import psycopg2 as ps
-from src.soporte_limpieza import info_df
 from src.soporte_carga import conexion_BBDD, carga_tabla_ciudad, carga_tabla_clientes, carga_tabla_eventos, carga_tabla_hoteles, carga_tabla_reservas
 import os
 from dotenv import load_dotenv
@@ -11,14 +10,15 @@ load_dotenv()
 archivo_limpio = os.getenv("archivo_limpio")
 archivo_extraccion = os.getenv("archivo_extraccion")
 
-data_hoteles = pd.read_parquet(archivo_limpio, parse_dates=['fecha_reserva', 'inicio_estancia', 'final_estancia'], dtype={'id_cliente': str, 'id_hotel': str})
+
+data_hoteles = pd.read_csv(archivo_limpio, parse_dates=['fecha_reserva', 'inicio_estancia', 'final_estancia'], dtype={'id_cliente': str, 'id_hotel': str})
 data_eventos = pd.read_csv(archivo_extraccion, parse_dates=['fecha_inicio', 'fecha_fin'])
 
-print(data_hoteles)
 
-#if __name__ == "__main__":
-#    conexion_BBDD("BBDD_Hoteles")
-#    carga_tabla_ciudad()
+
+if __name__ == "__main__":
+    conn = conexion_BBDD("BBDD_Hoteles")
+    carga_tabla_ciudad(conn)
 #    carga_tabla_eventos()
 #    carga_tabla_hoteles()
 #    carga_tabla_clientes()
