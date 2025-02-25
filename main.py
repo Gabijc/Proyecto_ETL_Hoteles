37a491d9e1351f48fabb346dcc77fee0fa811a0b
Gabriela Jiménez Conde <gabrielajimenezconde@gmail.com>
2025-02-25 21:31:09 +0100

    seguimos con el main

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from src.soporte_limpieza import info_df, data_fechas, cambio_id, limpieza_inicial
+ 
+
+if __name__ == "__main__":
+    limpieza_inicial(df_raw, hoteles_competencia_scrapeado)
