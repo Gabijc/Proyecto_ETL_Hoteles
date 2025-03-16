62c6a246329e53e1883af92814253cfaaba8c5d2
Gabriela Jiménez Conde <gabrielajimenezconde@gmail.com>
2025-03-16 19:57:58 +0100

    iniciamos dashboard streamlit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st  # type: ignore
+
+# Configuración de página
+st.set_page_config(page_title="Mi App", 
+									 layout="wide")
+
+
+# Mostrar un título en la aplicación
+st.title("Bienvenidos a mi aplicación 🎉")
